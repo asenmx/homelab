775cae2ee200d41fca7fda22e802b95fb9911865
Asen Marikostenliev <asen.marikostenliev@gmail.com>
2025-12-19 04:15:30 +0200

    add argocd helm install

--- talos-tf/helm.tf
+++ talos-tf/helm.tf
@@ -61,4 +61,12 @@ resource "helm_release" "cilium" {
     value = "true"
   }
 }
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "9.1.9"
+  namespace        = "argocd"
+  create_namespace = true
+}
 
